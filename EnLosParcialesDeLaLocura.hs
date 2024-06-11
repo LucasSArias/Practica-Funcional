c04fb124d517cca248f5a2a879a1cbe08126a950
LucasArias <lucaarias@frba.utn.edu.ar>
2024-06-11 17:39:35 -0300

    Hasta punto 4 completo

--- EnLosParcialesDeLaLocura.hs
+++ EnLosParcialesDeLaLocura.hs
@@ -15,6 +15,7 @@ mayorSegun f a b
     | f a > f b = a
     | otherwise = b
 
+deltaSegun :: Num a => (b -> a) -> (b -> b) -> b -> a
 deltaSegun ponderacion transformacion valor = abs ((ponderacion.transformacion) valor - ponderacion valor)
 
 -- PUNTO 1
@@ -32,3 +33,43 @@ agregarItem :: Item -> Investigador -> Investigador
 agregarItem unItem unInvestigador = unInvestigador {items = unItem : items unInvestigador}
 
 -- PUNTO 2
+algunoTieneElItem :: Item -> [Investigador] -> Bool
+algunoTieneElItem unItem = any (elem unItem. items) 
+
+-- PUNTO 3
+elLider :: [Investigador] -> Investigador
+elLider = maximoSegun potencial 
+
+potencial :: Investigador -> Int
+potencial unInvestigador 
+    | estaLoco unInvestigador = 0
+    | otherwise               = cordura unInvestigador * experiencia unInvestigador + (valor.itemDeMaximoValor) unInvestigador
+
+experiencia :: Investigador -> Int
+experiencia unInvestigador = 1 + (length.sucesosEvitados) unInvestigador 
+
+estaLoco :: Investigador -> Bool
+estaLoco unInvestigador = cordura unInvestigador == 0
+
+itemDeMaximoValor :: Investigador -> Item
+itemDeMaximoValor unInvestigador = maximoSegun valor (items unInvestigador)
+
+-- PUNTO 4
+-- A)
+deltaEnCorduraTotal :: Int -> [Investigador] -> Int
+deltaEnCorduraTotal unaCantidad =sum . map (deltaSegun cordura (enloquecer unaCantidad)) 
+
+-- B)
+deltaEnPotencial :: [Investigador] -> Int
+deltaEnPotencial = deltaSegun (head.map potencial) (filter (not.estaLoco))
+
+-- PUNTO 5
+data Suceso = Suceso {
+    descripcion :: String,
+    consecuencias :: [Consecuencia],
+    formaDeEvitarlo :: Forma
+}
+
+type Consecuencia = [Investigador] -> [Investigador]
+type Forma = [Investigador] -> Bool
+
